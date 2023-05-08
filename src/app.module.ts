f8445d30abbb13753670421e87cf80df147a14dd
juanmilobernal <juanmilobernal@gmail.com>
2023-05-08 09:48:41 -0500

    Update mongodb user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { BooksModule } from './books/books.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BooksModule, MongooseModule.forRoot('mongodb+srv://juanmilobernal:TemFyHIdWMXWykur@cluster0.3cikcby.mongodb.net/bookstore?retryWrites=true&w=majority')],
+  imports: [BooksModule, MongooseModule.forRoot('mongodb+srv://bernal:Uy3wNOFbL7VOTgwR@cluster0.3cikcby.mongodb.net/bookstore?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
