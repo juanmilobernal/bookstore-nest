7af3dc637d1f49210ab969b0bf5f9ee1cd87007c
juanmilobernal <juanmilobernal@gmail.com>
2023-05-15 12:53:01 -0500

    fix unit test

--- src/authorization/authorization.module.ts
+++ src/authorization/authorization.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BooksController } from 'src/books/books.controller';
+import { BooksModule } from 'src/books/books.module';
+import { BooksService } from 'src/books/books.service';
 
-@Module({})
+@Module({
+    imports: [BooksModule],
+    providers: [BooksService],
+    controllers: [BooksController],
+    exports: [AuthorizationModule]
+})
 export class AuthorizationModule {}
--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Book } from './schemas/book.schema';
+import { AuthorizationGuard } from 'src/authorization/authorization.guard';
+import { ConfigModule } from '@nestjs/config';
 
 describe('BooksController', () => {
-  let controller: BooksController;
+  let booksController: BooksController;
 
   beforeEach(async () => {
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       controllers: [BooksController],
+      providers: [BooksService, AuthorizationGuard,{
+        provide: getModelToken(Book.name),
+        useClass: jest.fn(),
+      }],
     }).compile();
 
-    controller = module.get<BooksController>(BooksController);
+    booksController = module.get<BooksController>(BooksController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(booksController).toBeDefined();
   });
 });
