5fb61dded1ea72ce665ecee6cb75230f0dc41e39
juanmilobernal <juanmilobernal@gmail.com>
2023-05-15 13:17:44 -0500

    fix authorization module instance

--- src/authorization/authorization.module.ts
+++ src/authorization/authorization.module.ts
@@ -1,12 +1,4 @@
 import { Module } from '@nestjs/common';
-import { BooksController } from 'src/books/books.controller';
-import { BooksModule } from 'src/books/books.module';
-import { BooksService } from 'src/books/books.service';
 
-@Module({
-    imports: [BooksModule],
-    providers: [BooksService],
-    controllers: [BooksController],
-    exports: [AuthorizationModule]
-})
+@Module({})
 export class AuthorizationModule {}
